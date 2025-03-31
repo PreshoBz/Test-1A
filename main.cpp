f48e1df490805f8a687249e9d6678fde686274c6
PreshoBz <2009118831@ub.edu.bz>
2025-03-31 13:50:32 -0600

    Added insert function into main

--- main.cpp
+++ main.cpp
@@ -5,5 +5,16 @@ using namespace std;
 int main() {
     BinaryTree tree;
 
+    tree.insert(50);
+    tree.insert(30);
+    tree.insert(70);
+    tree.insert(20);
+    tree.insert(40);
+    tree.insert(60);
+    tree.insert(80);
+
+    
+
     return 0;
 }
+
